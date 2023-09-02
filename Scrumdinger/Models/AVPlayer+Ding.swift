86dd490472eeaa790857ac338ef3ccbd99700bda
Marcus Gugacs <iimpaq@proton.me>
2023-09-02 17:30:29 +0200

    Managed state and life cycle

---
+++ Scrumdinger/Models/AVPlayer+Ding.swift
@@ -0,0 +1,13 @@
+/*
+See 3RD-PARTY-LICENSES.md file for this sample’s licensing information.
+*/
+
+import Foundation
+import AVFoundation
+
+extension AVPlayer {
+    static let sharedDingPlayer: AVPlayer = {
+        guard let url = Bundle.main.url(forResource: "ding", withExtension: "wav") else { fatalError("Failed to find sound file.") }
+        return AVPlayer(url: url)
+    }()
+}
